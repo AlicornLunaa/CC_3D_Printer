3c3fe3a493079968cf2ff47b0f79ad8ec497ec32
AlicornLunaa <alicornlunaa@gmail.com>
2023-06-25 10:49:30 -0500

    New workflow

--- back_end/src/index.ts
+++ back_end/src/index.ts
@@ -1,12 +1,11 @@
-import Schematic from "mc-schematic";
 import fs from "fs";
 
 /**
  * TODO: Convert to using NBT reader instead
  */
 
-fs.readFile('test/test1.schematic', (err, data) => {
-    Schematic.parse(data, (err: any, schem: any) => {
-        console.log(schem.getBlock(0, 0, 0));
-    });
+fs.readFile("./assets/test_schem.schem", (err, data) => {
+    // Schematic.parse(data, (err: any, schem: any) => {
+    //     console.log(schem.getBlock(0, 0, 0));
+    // });
 });
