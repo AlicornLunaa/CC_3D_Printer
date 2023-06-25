a65874f83451957c1bf58a26a6216dabbcdbe21c
AlicornLunaa <alicornlunaa@gmail.com>
2023-06-25 18:21:05 -0500

    Updated

--- back_end/src/index.ts
+++ back_end/src/index.ts
@@ -1,12 +1,34 @@
-
+import { WebSocketServer } from "ws";
 import Schematic from "./Schematic";
 import fs from "fs";
+
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on("connection", (socket) => {
+    socket.on("message", (data) => {
+        console.log(data.toString());
+    });
+
+    socket.on("close", () => {
+        console.log("Dropped client");
+    });
+
+    socket.on("error", (err) => {
+        console.error(err);
+    })
+
+    console.log("Found client");
+});
+
+wss.on("listening", () => {
+    console.log(`Websocket server listening`);
+});
 
 async function init(){
     let data = fs.readFileSync("./assets/test_schem.schem");
     let schem = await Schematic.parse(data);
 
-    console.log(schem.getBlock(5, 3, 7));
+    // console.log(schem.getBlock(5, 3, 7));
 }
 
 init();
