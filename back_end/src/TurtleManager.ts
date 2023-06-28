a6627ac7afbb33eedd69fb09dbab56df7416bcd8
AlicornLunaa <alicornlunaa@gmail.com>
2023-06-27 20:14:40 -0500

    Added basic RESTful api control over the turtle

---
+++ back_end/src/TurtleManager.ts
@@ -0,0 +1,48 @@
+import { WebSocketServer } from "ws";
+import { Server } from "http";
+import Turtle from "./Turtle";
+
+class TurtleManager {
+    // Variables
+    private socketServer: WebSocketServer;
+    private turtles: Turtle[];
+
+    // Functions
+    constructor(server: Server){
+        // Init variables
+        this.socketServer = new WebSocketServer({ server: server });
+        this.turtles = [];
+
+        // Connection handler
+        this.socketServer.on("connection", async (socket) => {
+            // Vars
+            const turtleIndex = this.turtles.length;
+        
+            // Events
+            socket.on("message", (data) => {});
+        
+            socket.on("close", () => {
+                console.log("Dropped client");
+                this.turtles.splice(turtleIndex, 1);
+            });
+        
+            socket.on("error", (err) => {
+                console.error(err);
+            })
+        
+            // Initializer
+            console.log("Found client");
+            this.turtles.push(new Turtle(socket));
+        });
+    }
+
+    public getTurtleCount(): number {
+        return this.turtles.length;
+    }
+
+    public getTurtle(index: number): Turtle {
+        return this.turtles[index];
+    }
+}
+
+export default TurtleManager;
--- back_end/src/index.ts
+++ back_end/src/index.ts
@@ -1,42 +1,71 @@
-import { WebSocketServer } from "ws";
+import TurtleManager from "./TurtleManager";
 import Schematic from "./Schematic";
+import bodyParser from "body-parser";
+import express from "express";
+import http from "http";
 import fs from "fs";
-import Turtle from "./Turtle";
 
-const wss = new WebSocketServer({ port: 8080 });
-let turtles: Turtle[] = [];
+const expressServer = express();
+const httpServer = http.createServer(expressServer);
+const turtleManager = new TurtleManager(httpServer);
 
-wss.on("connection", async (socket) => {
-    // Vars
-    const turtleIndex = turtles.length;
+// Init server
+expressServer.use(bodyParser.urlencoded({ extended: true }));
+expressServer.use(bodyParser.json());
 
-    // Events
-    socket.on("message", (data) => {});
+expressServer.use("/", express.static("front_end"));
 
-    socket.on("close", () => {
-        console.log("Dropped client");
-        turtles.splice(turtleIndex, 1);
-    });
+expressServer.get("/turtle/:id/:command", async (req, res) => {
+    let id = Number.parseInt(req.params.id);
+    let command = req.params.command;
+    let turtle = turtleManager.getTurtle(id);
 
-    socket.on("error", (err) => {
-        console.error(err);
-    })
+    let evaluateBool = (v: boolean) => {
+        if(v)
+            return "true";
 
-    // Initializer
-    console.log("Found client");
-    turtles.push(new Turtle(socket));
+        return "false";
+    }
+
+    switch(command){
+        case "forward":
+            res.end(evaluateBool(await turtle.forward()));
+            return;
+
+        case "back":
+            res.end(evaluateBool(await turtle.back()));
+            return;
+
+        case "up":
+            res.end(evaluateBool(await turtle.up()));
+            return;
+
+        case "down":
+            res.end(evaluateBool(await turtle.down()));
+            return;
+
+        case "turnLeft":
+            res.end(evaluateBool(await turtle.turnLeft()));
+            return;
+
+        case "turnRight":
+            res.end(evaluateBool(await turtle.turnRight()));
+            return;
+
+        default:
+            res.status(400).end();
+            return;
+    }
 });
 
-wss.on("listening", () => {
-    // Ready-up
-    console.log(`Websocket server listening`);
+httpServer.listen(8080, () => {
+    console.log("Listening on port 8080");
 });
 
-async function init(){
+// Functions
+async function initSchematic(){
     let data = fs.readFileSync("./assets/test_schem.schem");
     let schem = await Schematic.parse(data);
-
     // console.log(schem.getBlock(5, 3, 7));
 }
-
-init();
+initSchematic();
